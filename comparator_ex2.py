9a2d71668abc6c0ec2781f0d8f1291192d44dbf1
HoangTheViet <theviet050804@gmail.com>
2024-10-25 00:17:34 +0300

    first commit

--- comparator_ex2.py
+++ comparator_ex2.py
@@ -4,9 +4,9 @@ import ex2_without_re
 text1 = """Довольно распространённая ошибка ошибка – это лишний повтор повтор слова слова. Смешно, не не правда ли? Не нужно портить хор хоровод."""
 text2 = "alo alo, can you hear me?"
 text3 = "asdf asdf asdf asdf asdf\naha\nasdf"
-text4 = """For once, my dog ate my homework and I told my story to my teacher.
+text4 = """For once, my dog ate ate ate ate my homework and I told my story to my teacher.
            She laughed and said that: "That was tasty assignment, right?
-           I also joked: :Yes, it was was ;
+           I also joked: Yes, it was was;
            And at the end, I received a zero."""
 text5 = """After passing the test with flying colors, I screammmmmmm: Huraa Huraa Huraa """
 
