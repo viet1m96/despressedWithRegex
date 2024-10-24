5fac724eeab35f020fa1e2d58296e5d6bd5b32fc
HoangTheViet <theviet050804@gmail.com>
2024-10-25 00:10:13 +0300

    first commit

--- ex3.py
+++ ex3.py
@@ -1,10 +1,4 @@
-import functools
 import re
-
-def cmp(a: str, b: str) -> bool:
-    if len(a) < len(b): return True
-    if len(a) > len(b): return False
-    return a < b
 
 def process(test):
     pattern = r'\b[^euoaiEUOAI\s]*([euoaiEUOAI])([^euoaiEUOAI\s]*\1*)*\b'
@@ -17,9 +11,16 @@ def process(test):
     print()
 
 text1 = "Today was a good day for me, I wanna to flyyyyyy."
-text2 = "aaaa ae asdfasdfasdf asdf hihi"
+text2 = "aaaa ae asdfasdfasdf asdf hihi aeouif"
 text3 = "Do YoU want to build a snowmannn? Elsa asked"
-text4 = "Happy new year Happyyy birthday, Haloween day"
+text4 = """(Yo, left, yo, left) 'cause sometimes you just feel tired, feel weak
+(Yo, left, right, left) and when you feel weak
+(Yo, left, yo, left) you feel like you wanna just give up
+(Yo, left, right, left) but you gotta search within you
+(Yo, left, yo, left) try to find that inner strength and just pull that shit out of you
+(Yo, left, right, left) and get that motivation to not give up
+(Yo, left, yo, left) and not be a quitter, no matter how bad
+(Yo, left, right, left) you wanna just fall flat on your face and collapse"""
 text5 = "Look, if you had one shot or one opportunity. To seize everything you ever wanted in one moment. Would you capture it or just let it slip?"
 
 process(text1)
